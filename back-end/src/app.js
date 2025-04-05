e97ef6ee7d4f52a191b8d4f15c9d06ae5254cb6e
gabriel aleixo <aleixo.gabriel.aleixo@gmail.com>
2025-04-04 21:43:35 -0300

    Criação PRISMA e ROUTES, atualizando o app.js no back

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -15,4 +15,23 @@ app.use(cookieParser())
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
+//////////////////////////////////////////////////////////////Rotas
+
+import EmpresasRouter from './routes/empresas.js'
+import VagasRouter from './routes/vagas.js'
+import AlunoRouter from './routes/alunos.js'
+import PlanosRouter from './routes/planos.js'
+import CursosRouter from './routes/cursos.js'
+import ProfessoresRouter from './routes/professores.js'
+
+app.use('/empresas', EmpresasRouter)
+app.use('/vagas', VagasRouter)
+app.use('/alunos', AlunoRouter)
+app.use('/planos', PlanosRouter)
+app.use('/cursos', CursosRouter)
+app.use('/professores', ProfessoresRouter)
+
+//////////////////////////////////////////////////////////////
+
+
 export default app
