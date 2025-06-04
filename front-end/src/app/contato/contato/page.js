a0a3fab610033af78748604032ec7fe19affabf6
gabriel aleixo <aleixo.gabriel.aleixo@gmail.com>
2025-06-03 21:32:58 -0300

    revisão

---
+++ front-end/src/app/contato/contato/page.js
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+import NavBar from '@/components/Header/NavBar';
+
+export default function ContatoPage2() {
+    return (
+        <div className="">
+            <h1>Contato2</h1>
+            <p>Entre em contato conosco através do e-mail: </p>
+        </div>
+    )
+}
---
+++ front-end/src/app/contato/page.js
@@ -0,0 +1,11 @@
+'use client';
+import React from 'react';
+
+export default function ContatoPage() {
+    return (
+        <div className="divtotal">
+            <h1>Contato</h1>
+            <p>Entre em contato conosco através do e-mail: </p>
+        </div>
+    )
+}
